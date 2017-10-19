d0ca36c559f91757aec13c492104d938ce6bb9f0
marius851000 <mariusdavid@laposte.net>
2017-10-19 13:31:57 +0200

    update compile & gitignore

--- compile.bat
+++ compile.bat
@@ -1,1 +1,2 @@
+ppmd_statsutil.exe -i -scripts -romroot "rom" "export"
 ndstool.exe -c rom-mod.nds -9 rom/arm9.bin -7 rom/arm7.bin -y9 rom/y9.bin -y7 rom/y7.bin -d rom/data -y rom/overlay -t rom/banner.bin -h rom/header.bin
