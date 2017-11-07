8158224792986689db786b2257fcb34a7f80a4cd
marius851000 <mariusdavid@laposte.net>
2017-11-07 09:52:42 +0100

    Revert "Revert "added live""
    This reverts commit 5134fa6c417e778df6822ee2492165498f6c0a5f.

--- exportRead.py
+++ exportRead.py
@@ -267,7 +267,7 @@ def getData(file):
                             elif ltag == "EndAnimation":
                                 inlive.append({"lcommande" : "endAnimation"})
                             elif ltag == "SetBlink":
-                                inlive.appedn({"lcommande" : "setBlink",
+                                inlive.append({"lcommande" : "setBlink",
                                     "param" : lcommand.get("param"),
                                     "param_1" : lcommand.get("param_1")})
                             else:
